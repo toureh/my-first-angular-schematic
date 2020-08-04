835f18f8cb1d66148efee022012c51da73d434fe
Touré Holder <toureh@hotmail.com>
2020-08-04 18:16:08 -0300

    feat: use a template

--- src/hello/index.ts
+++ src/hello/index.ts
@@ -1,14 +1,26 @@
-import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  url,
+  apply,
+  template,
+  mergeWith,
+} from "@angular-devkit/schematics";
 import { Schema } from "./schema";
+import { strings } from "@angular-devkit/core";
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
 export function hello(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    const { name } = _options;
+    const sourceTemplates = url("./files");
 
-    tree.create("hello.js", `console.log('Hello ${name}!');`);
+    const sourceParamatrizedTemplates = apply(sourceTemplates, [
+      template({
+        ..._options,
+        ...strings,
+      }),
+    ]);
 
-    return tree;
+    return mergeWith(sourceParamatrizedTemplates)(tree, _context);
   };
 }
