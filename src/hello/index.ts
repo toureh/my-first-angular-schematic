10664cf279d974b73fe4258b62d76b1fc359561b
Touré Holder <toureh@hotmail.com>
2020-08-05 12:00:56 -0300

    feat: integrate with angular cli

--- src/hello/index.ts
+++ src/hello/index.ts
@@ -6,19 +6,44 @@ import {
   apply,
   template,
   mergeWith,
+  SchematicsException,
+  move,
 } from "@angular-devkit/schematics";
+
 import { Schema } from "./schema";
+
 import { strings } from "@angular-devkit/core";
+
+import { parseName } from "@schematics/angular/utility/parse-name";
+import { buildDefaultPath } from "@schematics/angular/utility/project";
 
 export function hello(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    const workspaceConfigBuffer = tree.read("angular.json");
+
+    if (!workspaceConfigBuffer) {
+      throw new SchematicsException("Not an Angular CLI workspace");
+    }
+
+    const workspaceConfig = JSON.parse(workspaceConfigBuffer.toString());
+    const projectName = _options.project || workspaceConfig.defaultProject;
+    const project = workspaceConfig.projects[projectName];
+
+    const defaultProjectPath = buildDefaultPath(project);
+
+    const parsedPath = parseName(defaultProjectPath, _options.name);
+
+    const { name, path } = parsedPath;
+
     const sourceTemplates = url("./files");
 
     const sourceParamatrizedTemplates = apply(sourceTemplates, [
       template({
         ..._options,
         ...strings,
+        name,
       }),
+      move(path),
     ]);
 
     return mergeWith(sourceParamatrizedTemplates)(tree, _context);
